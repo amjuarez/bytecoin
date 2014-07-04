879a5e40e01b561635d33054c456099000387145
ducknote <ducknote@users.noreply.github.com>
2014-07-05 00:34:38 +0400

    Update Checkpoints
    Update Checkpoints

--- src/cryptonote_core/checkpoints_create.h
+++ src/cryptonote_core/checkpoints_create.h
@@ -12,7 +12,10 @@
 namespace cryptonote {
   inline bool create_checkpoints(cryptonote::checkpoints& checkpoints)
   {
-
+    ADD_CHECKPOINT(1100,  "990a83b3e77ba5def86311da34793e09fa3b0a2875571bd59449173fddf4e129");
+    ADD_CHECKPOINT(4200,  "76af92fc41eadf9c99df91efc08011d0fce6f3f55b131da2449c187f432f91f7");
+    ADD_CHECKPOINT(11000, "970c15459e4d484166c36e303fcf35886e14633b40b1fe4e3f250eb03eaca1f8");
+    
     return true;
   }
 }
