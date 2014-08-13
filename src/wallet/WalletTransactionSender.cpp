ce6a3be6469c98b0a16f593baddfd31394d4a5d8
Antonio Juarez <antonio.maria.juarez@live.com>
2014-08-13 11:38:35 +0100

    Update license from MIT to LGPL

--- src/wallet/WalletTransactionSender.cpp
+++ src/wallet/WalletTransactionSender.cpp
@@ -1,9 +1,19 @@
-/*
- * WalletTransactionSender.cpp
- *
- *  Created on: 18 июня 2014 г.
- *      Author: milo
- */
+// Copyright (c) 2012-2014, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "WalletTransactionSender.h"
 #include "WalletUtils.h"
