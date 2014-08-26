8f19818ee24746bb30887f9638e2ecd038216109
ducknote <ducknote@users.noreply.github.com>
2014-08-26 16:41:10 +0400

    Default fee changed
    Default and minimum XDN fee changed from 0.1 XDN to 0.001 XDN

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -20,8 +20,8 @@
 #define MONEY_SUPPLY                                    UINT64_C(858986905600000000)
 // COIN - number of smallest units in one coin
 #define COIN                                            ((uint64_t)100000000) // pow(10, 8)
-#define DEFAULT_FEE                                     ((uint64_t)10000000) // pow(10, 7)
-#define MINIMUM_FEE                                     ((uint64_t)10000000) // pow(10, 7)
+#define DEFAULT_FEE                                     ((uint64_t)100000) // pow(10, 5)
+#define MINIMUM_FEE                                     ((uint64_t)100000) // pow(10, 5)
 
 #define START_BLOCK_REWARD                              (UINT64_C(320000) * COIN)
 #define MIN_BLOCK_REWARD                                (UINT64_C(150) * COIN)
