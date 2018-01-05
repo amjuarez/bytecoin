24df1558b127126415645cff885ce56a85c5a03a
RockSteady <34389545+RocksteadyTC@users.noreply.github.com>
2018-01-05 15:32:21 -0600

    m_verifivation_failed=>m_verification_failed
    typo

--- src/CryptoNoteCore/VerificationContext.h
+++ src/CryptoNoteCore/VerificationContext.h
@@ -22,7 +22,7 @@ namespace CryptoNote
   struct block_verification_context
   {
     bool m_added_to_main_chain;
-    bool m_verifivation_failed; //bad block, should drop connection
+    bool m_verification_failed; //bad block, should drop connection
     bool m_marked_as_orphaned;
     bool m_already_exists;
     bool m_switched_to_alt_chain;
